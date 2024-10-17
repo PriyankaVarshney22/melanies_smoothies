0d3b2dce3bd584de68e6ee446d003c399d74914b
PriyankaVarshney22 <priyankavaarshney22@gmail.com>
2024-10-17 23:35:59 +0530

    Update streamlit_app.py
    Added indent from 38 to 43

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,12 +35,12 @@ if ingredients_list:
             values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
 
     #st.write(my_insert_stmt)
-#st.stop()
-time_to_insert = st.button('Submit Order')
+    #st.stop()
+    time_to_insert = st.button('Submit Order')
 
-if time_to_insert:
-    session.sql(my_insert_stmt).collect()
-    st.success('Your Smoothie is ordered, '+ name_on_order +'!', icon="✅")
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered, '+ name_on_order +'!', icon="✅")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")

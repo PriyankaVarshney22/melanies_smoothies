bef50ef1c55d4a19215d07bdc69cdc7d97930b4e
PriyankaVarshney22 <priyankavaarshney22@gmail.com>
2024-10-17 22:40:55 +0530

    Update streamlit_app.py
    line 28 - added space between =

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ ingredients_list = st.multiselect(
 #ingredients_string=''
 if ingredients_list:
     
-    ingredients_string=''
+    ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         

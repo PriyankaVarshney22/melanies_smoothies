23e4fdff3f0f768534ec60f928be5877bf32d48c
PriyankaVarshney22 <priyankavaarshney22@gmail.com>
2024-10-17 23:54:54 +0530

    Update streamlit_app.py
    copied code again with indent changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -12,8 +13,7 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-cnx = st.connection("snowflake")
-session = cnx.session()
+session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
@@ -22,10 +22,9 @@ ingredients_list = st.multiselect(
     my_dataframe,
     max_selections=5
 )
-#ingredients_string = ''
 if ingredients_list:
     
-    ingredients_string = ''
+    ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
@@ -36,12 +35,8 @@ if ingredients_list:
 
     #st.write(my_insert_stmt)
     #st.stop()
-    time_to_insert = st.button('Submit Order')
+     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+ name_on_order +'!', icon="✅")
-
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)

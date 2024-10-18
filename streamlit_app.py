303e843d3f98a7183a72d1787e9548d092be7b58
PriyankaVarshney22 <priyankavaarshney22@gmail.com>
2024-10-18 15:46:56 +0530

    Update streamlit_app.py
    added line 32 and modified 33

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        fruityvice_response = {'name': 'Watermelon', 'id': 25, 'family': 'Cucurbitacea', 'order': 'Cucurbitales'}
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = ({'name': 'Watermelon', 'id': 25, 'family': 'Cucurbitacea', 'order': 'Cucurbitales'} + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response, use_container_width=True)
         
     #st.write(ingredients_string)
